3662ff1497a72fdb375427a789165f25c3141c25
Tim Zimmermann <tim@linux4.de>
2024-04-27 17:25:23 +0200

    battery: s2mf301_fuelgage: Use CHARGE_FULL capacity for CHARGE_COUNTER

--- drivers/battery/fuelgauge/s2mf301_fuelgauge/s2mf301_fuelgauge.c
+++ drivers/battery/fuelgauge/s2mf301_fuelgauge/s2mf301_fuelgauge.c
@@ -1750,6 +1750,7 @@ static int s2mf301_fg_get_property(struct power_supply *psy,
 {
 	struct s2mf301_fuelgauge_data *fuelgauge = power_supply_get_drvdata(psy);
 	enum power_supply_ext_property ext_psp = (enum power_supply_ext_property) psp;
+	union power_supply_propval value;
 
 	switch ((int)psp) {
 	case POWER_SUPPLY_PROP_STATUS:
@@ -1933,7 +1934,8 @@ static int s2mf301_fg_get_property(struct power_supply *psy,
 		val->intval = fuelgauge->mode;
 		break;
 	case POWER_SUPPLY_PROP_CHARGE_COUNTER:
-		val->intval = fuelgauge->pdata->capacity_full * fuelgauge->raw_capacity;
+		psy_do_property("battery", get, POWER_SUPPLY_PROP_CHARGE_FULL, value);
+		val->intval = value.intval/1000 * fuelgauge->raw_capacity; //uAh
 		break;
 	case POWER_SUPPLY_PROP_ONLINE:
 		pr_info("[DEBUG]%s: POWER_SUPPLY_PROP_ONLINE\n", __func__);
